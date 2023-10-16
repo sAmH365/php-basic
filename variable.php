f2ec70e2f3940461b4290d10c48ad89eb6845b62
sAmH365 <npower5377@naver.com>
2023-10-16 17:34:47 +0900

    variable

---
+++ variable.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+            $name = "samh365";
+
+            echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s ".$name.",
+            when an unknown printer ".$name." took a galley ".$name." of type and scrambled it ".$name." to make a type ".$name." specimen book.
+            It has survived not only five centuries, but also the leap ".$name." into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release ".$name." of Letraset sheets containing Lorem Ipsum ".$name." passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        ?>
+    </body>
+</html>
