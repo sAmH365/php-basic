f0f59ab1cbff410cc7447ae784a5fc561a369d7e
sAmH365 <npower5377@naver.com>
2023-10-17 11:40:36 +0900

    conditional

---
+++ conditional.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Conditional</h1>
+<h2>if</h2>
+<?php
+echo '1<br>';
+if(false) {
+    echo '2-1<br>';
+} else {
+    echo '2-2<br>';
+}
+echo '3<br>';
+
+?>
+</body>
+</html>
